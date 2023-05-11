a82a4566d2ed1708a3c9aa90f3a65eb3c6205d91
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-05-11 16:10:34 -0500

    /foo add

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/foo', function () {
+  Artisan::call('storage:link');
+});
+
 Route::get('/', HomeController::class)->name('home');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
